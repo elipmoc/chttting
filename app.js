da8721783c310141b5b764626d63771245eca569
PyMkk <yuyakunnyu@gmail.com>
2017-11-29 01:12:06 +0900

    hogehoge

---
+++ app.js
@@ -0,0 +1,10 @@
+var sys = require('sys');
+var http = require('http');
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World!!');
+        response.end();
+    }
+).listen(80);
+sys.log('Server running at http://0.0.0.0:3000/');
