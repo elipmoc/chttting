d43c15165e32ac173d71f6c61d74f470aa530853
PyMkk <yuyakunnyu@gmail.com>
2017-11-29 01:35:06 +0900

    node.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var sys = require('sys');
 var http = require('http');
-var server = http.createServer(
-    function (request, response) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello World!!');
-        response.end();
-    }
-).listen(process.env.PORT || 3000);
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request',doRequest);
+
+function doRequest(req,res){
+  fs.readFile('index.html','utf-8',doReard);
+
+  function doReard(err,data){
+    res.writeHead(200,{'Content-Type':'text/html'})
+    res.write(data);
+    res.end();
+  }
+}
+server.listen(process.env.PORT || 3000);
 sys.log('Server running at http://0.0.0.0:3000/');
