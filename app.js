fe5111ab887e71f71e96be09eae5029b7990bbd5
*Luca <Luca@HighLambda.local>
2017-11-29 19:06:58 +0900

    imac

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var Ws = require('ws').Server;
-var wss = new ws({port:8080});
+var wss = new ws({port:80});
 wss.on('connection',function(ws){
   ws.on('message',function(message){
     console.log("%s",message);
