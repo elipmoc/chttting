936df67e3ce355f00cc20565cef733b68b586e6f
*Luca <Luca@HighLambda.local>
2017-11-29 19:32:03 +0900

    ho

--- app.js
+++ app.js
@@ -1,35 +1,28 @@
-var fs = require("fs");
-var server = require("http").createServer(function(req, res) {
-     res.writeHead(200, {"Content-Type":"text/html"});
-     var output = fs.readFileSync("./index.html", "utf-8");
-     res.end(output);
-}).listen(process.env.PORT || 80);
-var io = require("socket.io").listen(server);
+var http = require( 'http' ); // HTTPモジュール読み込み
+var socketio = require( 'socket.io' ); // Socket.IOモジュール読み込み
+var fs = require( 'fs' ); // ファイル入出力モジュール読み込み
 
-// ユーザ管理ハッシュ
-var userHash = {};
+// 3000番ポートでHTTPサーバーを立てる
+var server = http.createServer( function( req, res ) {
+    res.writeHead(200, { 'Content-Type' : 'text/html' }); // ヘッダ出力
+    res.end( fs.readFileSync('./index.html', 'utf-8') );  // index.htmlの内容を出力
+}).listen(process.env.PORT || 8080);
 
-// 2.イベントの定義
-io.sockets.on("connection", function (socket) {
+// サーバーをソケットに紐付ける
+var io = socketio.listen( server );
 
-  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
-  socket.on("connected", function (name) {
-    var msg = name + "が入室しました";
-    userHash[socket.id] = name;
-    io.sockets.emit("publish", {value: msg});
-  });
+// 接続確立後の通信処理部分を定義
+io.sockets.on( 'connection', function( socket ) {
 
-  // メッセージ送信カスタムイベント
-  socket.on("publish", function (data) {
-    io.sockets.emit("publish", {value:data.value});
-  });
+    // クライアントからサーバーへ メッセージ送信ハンドラ（自分を含む全員宛に送る）
+    socket.on( 'c2s_message', function( data ) {
+        // サーバーからクライアントへ メッセージを送り返し
+        io.sockets.emit( 's2c_message', { value : data.value } );
+    });
 
-  // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
-  socket.on("disconnect", function () {
-    if (userHash[socket.id]) {
-      var msg = userHash[socket.id] + "が退出しました";
-      delete userHash[socket.id];
-      io.sockets.emit("publish", {value: msg});
-    }
-  });
+    // クライアントからサーバーへ メッセージ送信ハンドラ（自分以外の全員宛に送る）
+    socket.on( 'c2s_broadcast', function( data ) {
+        // サーバーからクライアントへ メッセージを送り返し
+        socket.broadcast.emit( 's2c_message', { value : data.value } );
+    });
 });
