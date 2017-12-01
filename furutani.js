814ce2b71918335824e6be15548ee0831273ff5a
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-12-01 20:55:55 +0900

    cookieを汎用的に扱う関数を定義したcookie.jsを適用

--- furutani.js
+++ furutani.js
@@ -1,36 +1,28 @@
-
 //非表示ユーザーIDリストのクッキー名
 const h_uID_list_c_n="h_uID_list";
 
 //クッキーから非表示ユーザーIDを配列で得る
 function get_h_uID_list(){
-    let cookie_list=document.cookie.split(";");
-    for(var i=0;i<cookie_list.length;i++){
-        var cookie=cookie_list[i].split("=");
-        if(cookie[0].replace(/\s+/g,"")==h_uID_list_c_n){
-            return cookie[1].split("&");
-        }
-    }
+    let result=get_cookie(h_uID_list_c_n);
+    if(result!=undefined)
+        return result.split("&");
+    return undefined;
 }
 
 //非表示ユーザーIDの配列をクッキーに保存する
 function save_h_uID_list(h_uID_list){
-    let h_uID_str=h_uID_list_c_n+"=";
+    let value="";
     if(h_uID_list.length>=1){
-        h_uID_str+=h_uID_list[0];        
-    
-        for(var i=1;i<h_uID_list.length;i++){
-            h_uID_str+="&"+h_uID_list[i];
-        }
-    document.cookie=h_uID_str;    
+        value+=h_uID_list[0];        
+        for(var i=1;i<h_uID_list.length;i++)
+            value+="&"+h_uID_list[i];
+        save_cookie(h_uID_list_c_n,value);    
     }
 }
 
 //クッキーの非表示ユーザーIDリストの情報を削除する
 function delete_h_uID_list(){
-    let date= new Date();
-    date.setTime(date.getTime()-1);
-    document.cookie=h_uID_list_c_n+"=; expires="+date.toUTCString();
+    delete_cookie(h_uID_list_c_n);
 }
 
 //クッキーの非表示ユーザーIDリストに新たなIDを追加する
