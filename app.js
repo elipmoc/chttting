cdb836af5484a3dfe47bc1ab93f670b0f6a2d498
PyMkk <yuyakunnyu@gmail.com>
2017-12-15 19:28:57 +0900

    hogehoge

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var html = require('fs').readFileSync('index.html');
 var logging = require('fs').readFileSync('logging.js');
+/*const {Client} = require('pg');
+const client = new Client();
 
+await client.connect()
+const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+await client.end()
+*/
 var http = require('http').createServer(
     function (req, res) {
       var url = req.url;
