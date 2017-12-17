72c8c630473696a4381a890a3f502fcfcf369d46
PyMkk <yuyakunnyu@gmail.com>
2017-12-18 06:31:17 +0900

    hoge

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var html = require('fs').readFileSync('index.html');
-<<<<<<< HEAD
 var logging = require('fs').readFileSync('logging.js');
 /*const {Client} = require('pg');
 const client = new Client();
@@ -8,10 +7,6 @@ await client.connect()
 const res = await client.query('SELECT $1::text as message', ['Hello world!'])
 await client.end()
 */
-=======
-var logging =require('fs').readFileSync('logging.js');
-
->>>>>>> af051038ad84c45c7081d2512fb66f090db5d70e
 var http = require('http').createServer(
     function (req, res) {
       var url = req.url;
@@ -21,20 +16,12 @@ var http = require('http').createServer(
         } else if ('/logging.js' == url) {
           res.writeHead(200, {'Content-Type': 'text/plain'});
           res.end(logging);
-<<<<<<< HEAD
         }
     }
 );
 const io = require('socket.io')(http);
 const adminNamespace = io.of('/admin');
 
-=======
-        }          
-    }
-);
-
-var io = require('socket.io')(http);
->>>>>>> af051038ad84c45c7081d2512fb66f090db5d70e
 var webPort = process.env.PORT || 3000;
 
 http.listen(webPort,"0.0.0.0");
