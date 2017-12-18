dacf3a4e35615159f4fb4181d7c1d8b339a5bc07
PyMkk <yuyakunnyu@gmail.com>
2017-12-18 11:23:47 +0900

    hoge

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var html = require('fs').readFileSync('index.html');
 var logging = require('fs').readFileSync('logging.js');
-var {Pool,Client} = require('pg');
+var pg = require('pg');
 const postring = "postgres://cnoqoxqavuubfy:b831a9f787f1f394987277635cdcb73abf68cb73daeccc0b50cdadf95f83575c@ec2-54-83-194-208.compute-1.amazonaws.com:5432/dc8lm58eis0g00";
+var client = new pg.Client(postring);
+var resultName = "";
 
-const pool = new Pool ({
-  postring:postring,
+client.connect(function(err) {
+    if(err) {
+        return console.error('could not connect to postgres', err);
+    }
+    client.query('select *from juse;', function(err, result) {
+        if(err) {
+            return console.error('error running query', err);
+        }
+        console.log(result.rows[0].name);
+        console.log(result.rows[1].name);
+        resultName = result.rows[0].name;
+        client.end();
+
+    });
 });
 
-pool.query('SELECT NOW()',(err,res)=>{
-  console.log(err, res)
-pool.end();
-});
-
-const client = new Client({
-  postring:postring,
-});
-
-client.connect();
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-});
 
 var http = require('http').createServer(
     function (req, res) {
