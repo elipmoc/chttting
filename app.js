3c46a0bbb9f02f0644f1991bab42b5bdd0eb881c
PyMkk <yuyakunnyu@gmail.com>
2017-12-19 16:44:34 +0900

    hoge

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const client = new Client({
 
 client.connect();
 
-client.query('select rn from ten;',(err,res)=>{
+client.query("insert into ten values('55');",(err,res)=>{
     if (err) throw err;
   for(let row of res.rows){
     testStr += JSON.stringify(row);
