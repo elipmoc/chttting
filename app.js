05bab170048465fbf4f7da72dbb76d87e9b417e2
PyMkk <yuyakunnyu@gmail.com>
2017-12-21 18:05:03 +0900

    hoge

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ client.connect();
 client.query("select * from room;",(err,res)=>{
     if (err) throw err;
   for(let row of res.rows){
-    testStr += row;
+    testStr += JSON.stringify(row);
   }
   client.end();
 });
