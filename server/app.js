71deee7f8f62edb241adad8c135d5710e6171b4f
PyMkk <yuyakunnyu@gmail.com>
2018-01-19 15:04:16 +0900

    ルーム表示をするためのソケットを定義した

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var html = require('fs').readFileSync('main.html');
 var arrow = require('fs').readFileSync('commentArrow.js');
+var loadRoomJs = require('fs').readFileSync('loadRoomList.js');
 var filter = require('fs').readFileSync('commandFilter.js');
 var syamu = require('fs').readFileSync('syamu.html');
 var dip = require('fs').readFileSync('dip.html');
@@ -8,17 +9,16 @@ var logging = require('fs').readFileSync('logging.js');
 var chatConnection = require('fs').readFileSync('chatConnection.js');
 var logDB = require('./logDB.js');
 var sys = require('util');
-
 const {
     URL
 } = require('url');
 var qs = require('querystring');
 var pg = require('pg');
-
 const {
     Client
 } = require('pg');
 
+//データベースの接続設定
 var room_name_list = new Array();
 var testStr = "tintin";
 const client = new Client({
@@ -39,7 +39,7 @@ client.query("select room_name from room;", (err, res) => {
 
 
 var http = require('http').createServer(
-    function (req, res) {
+    function(req, res) {
         var url = req.url;
         if (req.method == 'GET') {
             var url_parts = new URL("https://serene-fjord-98327.herokuapp.com" + url);
@@ -89,6 +89,11 @@ var http = require('http').createServer(
                 'Content-Type': 'text/plain'
             });
             res.end(chatConnection);
+        } else if ("/loadRoomList.js" == url) {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end(loadRoomJs);
         }
     }
 );
@@ -97,21 +102,31 @@ const io = require('socket.io')(http);
 //名前空間のリスト。いまはまだ使いみちがない
 let namespaceList = new Array();
 
+function loadRoomSocket() {
+    let namespace = io.of("/loadRoomStream");
+    socket.on(
+        'loadRoom',
+        function(data) {
+            namespace.emit('loadRoom', JSON.stringify(room_name_list));
+        }
+    )
+}
+
 //クライアントソケットの応答処理
 function socketOn(namespace) {
-
-    return function (socket) {
+    return function(socket) {
         socket.on(
             'msg',
-            function (data) {
+            function(data) {
                 console.log("msg:" + data);
                 namespace.emit('msg', data);
                 logDB.logPush(namespace.name, data);
             }
         );
+
         socket.on(
             'initMsg',
-            function (data) {
+            function(data) {
                 console.log("initmsg:" + data);
                 socket.emit(
                     'initMsg',
@@ -122,10 +137,9 @@ function socketOn(namespace) {
     }
 }
 
-
 //roomNameListから各種ソケットの名前空間リストを生成
-function makenamespace() {
-    room_name_list.forEach(function (x) {
+function makeNameSpace() {
+    room_name_list.forEach(function(x) {
         let namespace = io.of("/" + x);
         namespace.on('connection', socketOn(namespace));
         namespaceList[x] = namespace;
@@ -137,10 +151,10 @@ var adminNamespace = io.of("/admin");
 http.listen(webPort);
 adminNamespace.on(
     'connection',
-    function (socket) {
+    function(socket) {
         socket.on(
             'msg',
-            function (data) {
+            function(data) {
                 adminNamespace.emit('msg', /*testStr*/ data + String(url_parts.query));
                 switch (url_parts.query) {
                     case "room_admin":
