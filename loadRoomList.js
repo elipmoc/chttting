b060c7b793eba457f54c0bf2cab525acaa2debca
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-19 18:43:43 +0900

    hoge

--- loadRoomList.js
+++ loadRoomList.js
@@ -1,7 +1,6 @@
 var socket = io("/loadRoomStream");
 socket.emit('loadRoom', "");
 socket.on('loadRoom', data => {
-      alert("hoge");
       JSON.parse(data).forEach(
             name => $('#roomList').prepend("<h1>" + name + "</h1>")
       );
