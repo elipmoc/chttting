ae7136d7bf8ea4e439785783256b72848a8a9189
PyMkk <yuyakunnyu@gmail.com>
2018-01-19 16:08:15 +0900

    ルームを表示する関数定義

--- loadRoomList.js
+++ loadRoomList.js
@@ -2,3 +2,8 @@ var socket = io("/loadRoomSocket");
 socket.on('loadRoomSocket', data) => {
     socket.emit('loadRoomSocket', data);
 };
+
+var loadRoomName = new loadRoomName("loadRoom", roomNameData);
+function roomNameData(data){
+$('#roomList').prepend(data);
+}
