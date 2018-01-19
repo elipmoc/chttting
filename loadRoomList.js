02615e59760ebd48000d738332467b678e17af97
PyMkk <yuyakunnyu@gmail.com>
2018-01-19 17:35:32 +0900

    bug

--- loadRoomList.js
+++ loadRoomList.js
@@ -1,5 +1,5 @@
 var socket = io("/loadRoomStream");
 socket.on('loadRoom', data => {
       data = "<h1>" + data + "</h1>";
-      $('#roomList').prepend(data);
+      $('#roomList').prepend(JSON.stringify(data));
 });
