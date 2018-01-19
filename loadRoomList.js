f74ca20e438460f75335e05aa626f7312b8b44ac
PyMkk <yuyakunnyu@gmail.com>
2018-01-19 16:18:36 +0900

    バグ修正３

--- loadRoomList.js
+++ loadRoomList.js
@@ -1,9 +1,5 @@
 var socket = io("/loadRoomSocket");
 socket.on('loadRoomSocket', data => {
-    socket.emit('loadRoomSocket', data);
+      data = "<h1>" + data + "</h1>";
+      $('#roomList').prepend(data);
 });
-
-function roomNameData(data){
-data = "<h1>"+data+"</h1>";
-$('#roomList').prepend(data);
-}
