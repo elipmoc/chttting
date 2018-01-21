a2ac9e524d8ed21f0d4b76983e80c97b8b3e4862
PyMkk <yuyakunnyu@gmail.com>
2018-01-22 02:07:45 +0900

    hoge!

--- logging.js
+++ logging.js
@@ -6,12 +6,12 @@ $('#right').click(function(e) {
     document.location.href = "dip.html?name=dipe2";
 });
 
-socket.emit('debate_title', "");
-var debate_socket = io("/debateStream");
+//socket.emit('debate_title', "");
+//var debate_socket = io("/debateStream");
 var loc = document.location.href;
 var paramItem = loc.split('=');
 var chatConnection = new ChatConnection("dipe", msgDataAdd);
-var debateTitleSend = new ChatConnection("debate_title", odai);
+//var debateTitleSend = new ChatConnection("debate_title", odai);
 
 $('#ugo').click(function(e) {
     let ms = document.myf.com.value;
@@ -31,8 +31,8 @@ $('#ugo').click(function(e) {
 $("#odai").click(function(e) {
     debateTitleSend.sendData(
         JSON.stringify({
-            "dai": "<h2>" + document.myf.word.value + "</h2>";
-        });
+            "dai": "<h2>" + document.myf.word.value + "</h2>"
+        })
     );
 });
 
@@ -56,12 +56,12 @@ function msgDataAdd(data) {
         $('#chat_log2').prepend(msg);
     }
 }
-
+/*
 function odai(data){
   data = JSON.parse(data);
   $("#titlec").prepend(data);
 }
-
+*/
 /*
 socket.on('dai',
     function (data) {
