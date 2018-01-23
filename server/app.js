3e4febc26e39f225cf9829ebef0b5a180e53276e
PyMkk <yuyakunnyu@gmail.com>
2018-01-23 23:40:22 +0900

    hoge

--- server/app.js
+++ server/app.js
@@ -82,7 +82,7 @@ function chatSocket(namespace) {
 
 //roomNameListから各種ソケットの名前空間リストを生成  *
 function makeNameSpace() {
-    room_list.map("room_name").forEach((x)=> {
+    room_list.map(room.room_name {
         let namespace = io.of("/" + x);
         namespace.on('connection', chatSocket(namespace));
         namespaceList[x] = namespace;
