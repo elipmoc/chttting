b435fe4759cc6c7068fa878a998257d93d35153f
PyMkk <yuyakunnyu@gmail.com>
2018-01-24 15:08:46 +0900

    デザイン改良

--- Js/discussion.js
+++ Js/discussion.js
@@ -33,7 +33,7 @@ $('#chat_send').click(() => {
 function msgDataAdd(data) {
     data = JSON.parse(data);
     console.log(data);
-    let msg = '<div style="border-top:1px #D5D8DC solid; margin-top:-3px;margin-bottom:-3px;">'+ commandFilter(data["msg"]) + '</div><br>';
+    let msg = '<div style="border-top:1px #D5D8DC solid; margin-top-bottom:-3px;">'+ commandFilter(data["msg"]) + '</div><br>';
 
     if (data["dipeType"] == "debateLeft") {
         $('#chat_log').prepend(msg);
