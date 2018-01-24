be9866e733d0c4d84e03daaf77f20c85120c0026
PyMkk <yuyakunnyu@gmail.com>
2018-01-24 15:59:19 +0900

    firstAccessSocketを呼び出した

--- Js/firstAccessStream.js
+++ Js/firstAccessStream.js
@@ -2,5 +2,5 @@ const socket = io("/firstLoadStream");
 socket.emit('firstSend', "");
 socket.on('firstSend', data => {
     let titlein = document.getElementById("titlec");
-    title.innerHTML = data;
+    titlein.innerHTML = data;
 });
