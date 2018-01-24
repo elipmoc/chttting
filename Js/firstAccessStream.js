c1ccd906bdb3dea5b2dca6f62c37373a9464fba8
PyMkk <yuyakunnyu@gmail.com>
2018-01-24 18:18:17 +0900

    XSSの脆弱性削除

--- Js/firstAccessStream.js
+++ Js/firstAccessStream.js
@@ -2,5 +2,5 @@ const firstSocket = io("/firstLoadStream");
 firstSocket.emit('firstSend', "");
 firstSocket.on('firstSend', (data) => {
     let titlein = document.getElementById("titlec");
-    titlein.innerText = $("#titlec").text(data).html();
+    titlein.innerText = $("#titlec").text(data);
 });
