1536e06a75338a44c0f9ba3be68dfb51eb956577
PyMkk <yuyakunnyu@gmail.com>
2018-01-26 18:46:22 +0900

    hoge

--- Js/firstAccessStream.js
+++ Js/firstAccessStream.js
@@ -3,12 +3,8 @@ const urlLocation = document.location.href;
 const urlParam = urlGetParamParse(urlLocation);
 firstSocket.emit('firstSend', "");
 firstSocket.on('firstSend', (data) => {
-    JSON.parse(data).forEach(room => {
-        alert(room.room_name);
-        alert(room.debate_title);
-        if (room.room_name == urlParam["room_name"]) {
-            $("#titlec").text(room.debate_title).html();
+    data[urlParam["room_name"]];
 
-        }
-    });
+    $("#titlec").text(data[urlParam["room_name"]]).html();
+
 });
