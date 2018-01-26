9cfd7ac6020454a4bd65f9f911cca5cfa439b278
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-26 15:29:25 +0900

    部屋を作成失敗するとエラーを表示するようにした

--- server/app.js
+++ server/app.js
@@ -82,10 +82,16 @@ function roomCreateSocket() {
             data = JSON.parse(data);
             let roomName = escape(data["roomName"]);
             let roomType = escape(data["roomType"]);
-            createRoomDB.createRoom(roomName, roomType, () => {
-                addRoom(roomName, roomType);
+            createRoomDB.createRoom(roomName, roomType, (flag) => {
+                if (flag) {
+                    addRoom(roomName, roomType);
+                    socket.emit("created", "");
+                }
+                else {
+                    socket.emit("created", "部屋の作成に失敗しました");
+                }
             });
-            socket.emit("created", "");
+
         });
     });
 }
