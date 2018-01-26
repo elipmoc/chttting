e9b049409b34b0e24503829d0a30837e9ff5b426
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-26 18:34:10 +0900

    ログ30件越えたら削除するテスト

--- server/logDB.js
+++ server/logDB.js
@@ -18,10 +18,24 @@ exports.logPush = (roomNameSpace, msg) => {
         let id = res.rows[0]["room_id"];
         client.query("insert into msg (room_id,msg_data) values ($1,$2);", [Number(id), msg], (err, res) => {
             if (err) throw err;
-            client.end();
+            client.query("select count(*) from msg where room_id=$1;", [id], (err, res) => {
+                if (err) throw err;
+                if (res.rows[0]["count"] > 30) {
+                    client.query("select msg_id from msg offset 0 limit 1;", (err, res) => {
+                        if (err) throw err;
+                        client.query("delete from msg where msg_id=$1;", [res.rows[0]["msg_id"]], (err, res) => {
+                            if (err) throw err;
+                            client.end();
+                        });
+                    });
+                }
+                else
+                    client.end();
+            });
         });
     });
 }
+
 
 exports.logRead = (roomNameSpace, func) => {
     if (roomNameSpace[0] != '/') {
