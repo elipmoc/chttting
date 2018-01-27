69875dc7d10d2afc6b705d5395f3d56670531617
PyMkk <yuyakunnyu@gmail.com>
2018-01-27 12:13:48 +0900

    hoge

--- Js/loadRoomList.js
+++ Js/loadRoomList.js
@@ -12,7 +12,7 @@ socket.on('loadRoom', data => {
                     url = "normalChatRoom.html";
                     break;
             }
-            $('#card-list').prepend('<div class="card text-center"><div class="card-block"><h4 class="card-title">'+room.room_name+'</h4><p class="card-text">Description<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></p><a class="btn btn-primary" href="' + url + '?roomName=' + encodeURIComponent(room.room_name) + '"><font size="3">' + room.room_name + '</font></a>');
+            $('#card-list').prepend('<div class="card text-center"><div class="card-block"><h4 class="card-title">'+room.room_name+'</h4><p class="card-text">Description<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p></p><a class="btn btn-primary" href="' + url + '?roomName=' + encodeURIComponent(room.room_name) + '"><font size="3">' + room.room_name + '</font></a></div></div>');
         }
     );
 });
