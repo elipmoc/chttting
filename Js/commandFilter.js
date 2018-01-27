ffbdfb67e5f0e678eaa02122e45effd7596ddde9
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-27 13:05:59 +0900

    hscalcテスト

--- Js/commandFilter.js
+++ Js/commandFilter.js
@@ -4,6 +4,9 @@ function htmlEscape(htmlText) {
     return $('<div/>').text(htmlText).html();
 }
 
+//hscalcライブラリのロード
+//hscalc.load();
+
 //コマンドを読み取り実行結果を得る
 function commandFilter(data) {
     switch (true) {
@@ -13,6 +16,16 @@ function commandFilter(data) {
             return (data + '<img src="https://pbs.twimg.com/profile_images/510615322307461120/o-vKGUzY_400x400.jpeg" width="100" height="100">');
         case /810/.test(data):
             return "まだ実装されてないコマンドだよ";
+        case /hscalc/.test(data):
+            data = htmlEscape(data);
+            let userName = data.replace(/hscalc.*/g, "");
+            let msg = data.replace(/.*hscalc/g, "");
+            return userName + msg;
+        /*            let lexerRet = hscalc.lexer(data);
+                    if (lexerRet.errorFlag) {
+                        return 
+                    }*/
+
         default:
             data = htmlEscape(data);
             return data;
