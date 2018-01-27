f5732f816b5f829fee08e1c72b12d1c5eb8a530c
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-27 13:15:13 +0900

    scriptloadのパス修正

--- Js/hscalc/hscalc.js
+++ Js/hscalc/hscalc.js
@@ -10,11 +10,11 @@ hscalc.scriptload = (scriptName) => {
 };
 
 hscalc.load = () => {
-    hscalc.scriptload("VariableTable.js");
-    hscalc.scriptload("expressionTrees.js");
-    hscalc.scriptload("originalTypes.js");
-    hscalc.scriptload("intrinsicFuncTable.js");
-    hscalc.scriptload("operatorTable.js");
-    hscalc.scriptload("lexer.js")
-    hscalc.scriptload("parser.js");
+    hscalc.scriptload("Js/hscalc/VariableTable.js");
+    hscalc.scriptload("Js/hscalc/expressionTrees.js");
+    hscalc.scriptload("Js/hscalc/originalTypes.js");
+    hscalc.scriptload("Js/hscalc/intrinsicFuncTable.js");
+    hscalc.scriptload("Js/hscalc/operatorTable.js");
+    hscalc.scriptload("Js/hscalc/lexer.js")
+    hscalc.scriptload("Js/hscalc/parser.js");
 };
