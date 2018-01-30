c7e2a82bff4fec31d9723081c2ca0e54c83a4082
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-30 14:38:46 +0900

    不要なコードを削除した

--- server/discussionSocket.js
+++ server/discussionSocket.js
@@ -1,5 +1,6 @@
 let debate_title = {};
 
+//議題を定義するためのイベントをソケットにバインド
 exports.bindDiscussionSocket = (namespace) => {
     return (socket) => {
         socket.on("titleSend", (title) => {
@@ -8,17 +9,6 @@ exports.bindDiscussionSocket = (namespace) => {
             debate_title[title_data["room_name"]] = title_data["debate_title"];
         });
     };
-}
-
-//議題を定義するためのソケットを定義
-exports.debateTitleSocket = (mainSocket) => {
-    mainSocket.on("connection", (socket) => {
-        socket.on("titleSend", (title) => {
-            let title_data = JSON.parse(title);
-            socket.emit("titleSend", title_data["debate_title"]);
-            debate_title[title_data["room_name"]] = title_data["debate_title"];
-        });
-    });
 }
 
 exports.firstAccessSocket = (mainSocket) => {
