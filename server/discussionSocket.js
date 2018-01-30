9409e477b094dc9636bf738601b5f6391b6d7dd3
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-30 16:40:34 +0900

    議題を上書きできないようにした

--- server/discussionSocket.js
+++ server/discussionSocket.js
@@ -2,17 +2,24 @@
 exports.DiscussionNameSpace = class {
     constructor(namespace) {
         this._debate_title = "";
+        this._voteFlag = false;
         this.event = (socket) => {
             socket.on("titleSend", (title) => {
+                if (this._debate_title == "")
+                    return;
                 this._debate_title = title;
                 namespace.emit("titleSend", this._debate_title);
                 setTimeout(() => {
                     namespace.emit("startVote", "");
+                    this._voteFlag = true;
                 }, 30 * 1000);
             });
             socket.on("firstTitleSend", (data) => {
                 socket.emit("firstTitleSend", this._debate_title);
             });
+            socket.on("initVoteFlag", (data) => {
+                socket.emit("initVoteFlag", this._voteFlag);
+            })
         };
     }
 }
