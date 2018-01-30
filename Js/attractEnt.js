b1282094da4620b93a1a93b9761a43aa9e904d38
PyMkk <yuyakunnyu@gmail.com>
2018-01-30 15:13:27 +0900

    2hogegejfgiosjgiosjiogfes

--- Js/attractEnt.js
+++ Js/attractEnt.js
@@ -1,9 +1,9 @@
-const socket = io("attractConnection");
+const socket = io("/attractConnection");
 const url = location.href;
 $("#attract_send").click(() => {
   const atr_word = document.myf.attract_word.value;
   socket.emit("attractWrite");
   socket.on("attractWrite", (attractWord) => {
     $("#attract_box").prepend('<a href="' + urlLocation + '">'+attractWord+'</a>');
-  });
+  })
 });
