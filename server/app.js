289183ceac9120a90e6747c23552b76bcfc8a4f7
PyMkk <yuyakunnyu@gmail.com>
2018-01-30 23:46:49 +0900

    delete getIP

--- server/app.js
+++ server/app.js
@@ -38,21 +38,6 @@ function attractWriteSocket() {
   });
 }
 
-let getIP = function(req) {
-  if (req.headers['x-forwarded-for']) {
-    return req.headers['x-forwarded-for'];
-  }
-  if (req.connection && req.connection.remoteAddress) {
-    return req.connection.remoteAddress;
-  }
-  if (req.connection.socket && req.connection.socket.remoteAddress) {
-    return req.connection.socket.remoteAddress;
-  }
-  if (req.socket && req.socket.remoteAddress) {
-    return req.socket.remoteAddress;
-  }
-  return '0.0.0.0';
-};
 
 function testSocket() {
   const nm = io.of("/aaa");
