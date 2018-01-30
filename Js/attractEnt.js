9edc360fbb7256417bc5eb06e783c26d1517ff42
PyMkk <yuyakunnyu@gmail.com>
2018-01-30 16:00:59 +0900

    yasai

--- Js/attractEnt.js
+++ Js/attractEnt.js
@@ -2,7 +2,7 @@ const attract_socket = io("/attractConnection");
 const url = location.href;
 $("#attract_send").click(() => {
   const atr_word = document.myf.attract_word.value;
-  attract_socket.on("attractWrite",()=>{
-  attract_socket.emit("attractWrite",atr_word);
-});
+  attract_socket.on("attractWrite", (data) => {
+    attract_socket.emit("attractWrite", atr_word);
+  });
 });
