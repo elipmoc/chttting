9d5cab12a1b5e29b8c1807bb99fa7acd3452498a
PyMkk <yuyakunnyu@gmail.com>
2018-01-30 22:03:49 +0900

    party

--- server/app.js
+++ server/app.js
@@ -41,6 +41,22 @@ function attractWriteSocket() {
   });
 }
 
+let getIP = function (req) {
+  if (req.headers['x-forwarded-for']) {
+    console.log(req.headers['x-forwarded-for']);
+  }
+  if (req.connection && req.connection.remoteAddress) {
+    console.log(req.connection.remoteAddress);
+  }
+  if (req.connection.socket && req.connection.socket.remoteAddress) {
+    console.log(req.connection.socket.remoteAddress);
+  }
+  if (req.socket && req.socket.remoteAddress) {
+    console.log(req.socket.remoteAddress);
+  }
+  return '0.0.0.0';
+};
+
 /*function attractMainSocket() {
   const attractNamespace = io.of("/attractConnection");
 
