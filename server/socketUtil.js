6d9e0b6f183731af2919cb4bcf5a86ee646ca9a9
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-02-01 01:38:02 +0900

    socketUtil.jsの修正

--- server/socketUtil.js
+++ server/socketUtil.js
@@ -2,6 +2,6 @@
 exports.getClientIP = (socket) => {
     let ip = socket.handshake.headers['x-forwarded-for'];
     if (ip == undefined)
-        ip = socket.handshake.address;
+        ip = "localhost";
     return ip;
 }
