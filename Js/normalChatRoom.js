46f61d0cda9305c7ec50fdf59b1c2820630c244e
PyMkk <yuyakunnyu@gmail.com>
2018-02-02 15:02:09 +0900

    参加者実装

--- Js/normalChatRoom.js
+++ Js/normalChatRoom.js
@@ -3,29 +3,36 @@ const paramItem = loc.split('=');
 const chatConnection = new ChatConnection(decodeURIComponent(paramItem[1]), msgDataAdd);
 
 $("#com").keydown((e) => {
-    let ms = document.myf.com.value;
-    let nm = document.myf.name.value;
-
-    if (ms != "" && nm != "") {
-        if (e.keyCode == 13) {
-            chatConnection.sendData(nm + " > " + ms);
-            document.myf.com.value = "";
-        }
+  let ms = document.myf.com.value;
+  let nm = document.myf.name.value;
+  if (ms != "" && nm != "") {
+    chatConnection.setUserData(JSON.stringify({
+      name: nm,
+      dipeType: urlParam["stance"]
+    }));
+    if (e.keyCode == 13) {
+      chatConnection.sendData(nm + " > " + ms);
+      document.myf.com.value = "";
     }
+  }
 });
 
 $('#sendButton').click((e) => {
-    let ms = document.myf.com.value;
-    let nm = document.myf.name.value;
+  let ms = document.myf.com.value;
+  let nm = document.myf.name.value;
 
-    if (ms != "" && nm != "") {
-        chatConnection.sendData(nm + " > " + ms);
-    }
-    document.myf.com.value = "";
+  if (ms != "" && nm != "") {
+    chatConnection.setUserData(JSON.stringify({
+      name: nm,
+      dipeType: urlParam["stance"]
+    }));
+    chatConnection.sendData(nm + " > " + ms);
+  }
+  document.myf.com.value = "";
 });
 
 //データをチャットメッセージとして追加する関数
 function msgDataAdd(data) {
-    let msg = commandFilter(data) + '<br><hr>';
-    $('#chat_log').prepend(msg);
+  let msg = commandFilter(data) + '<br><hr>';
+  $('#chat_log').prepend(msg);
 }
