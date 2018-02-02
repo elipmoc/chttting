7c0d14bf57ffbb61bb933b1e5ba3f95dfab16a71
PyMkk <yuyakunnyu@gmail.com>
2018-02-02 14:32:31 +0900

    木村と三浦を実装した

--- Js/commandFilter.js
+++ Js/commandFilter.js
@@ -9,15 +9,25 @@ hscalc.load();
 //コマンドを読み取り実行結果を得る
 function commandFilter(data) {
   switch (true) {
-    case /931/.test(data):
+    case />.* 931/.test(data):
       data = data.replace(/931/g, "");
       data = htmlEscape(data);
       return (data + '<img src="https://pbs.twimg.com/profile_images/510615322307461120/o-vKGUzY_400x400.jpeg" width="100" height="100">');
 
-    case /810/.test(data):
+    case />.* 810/.test(data):
       data = data.replace(/810/g, "");
       data = htmlEscape(data);
       return (data + '<img src="https://pbs.twimg.com/profile_images/710311323561627648/5IbTKZ76.jpg" width="100" height="100">');
+
+    case />.* 1919/.test(data):
+      data = data.replace(/931/g, "");
+      data = htmlEscape(data);
+      return (data + '<img src="http://tn.smilevideo.jp/smile?i=20394692.L" width="100" height="100">');
+
+    case />.* 893/.test(data):
+      data = data.replace(/931/g, "");
+      data = htmlEscape(data);
+      return (data + '<img src="http://image01.seesaawiki.jp/b/i/bbsenpai/e118b95c81b903db.jpg" width="100" height="100">');
 
     case /hscalc/.test(data):
       let userName = htmlEscape(data.replace(/hscalc.*/g, ""));
