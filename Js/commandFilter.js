947676626ca822a21917f3fd31224458aea7866d
PyMkk <yuyakunnyu@gmail.com>
2018-02-02 14:17:02 +0900

    コマンドフィルター修正

--- Js/commandFilter.js
+++ Js/commandFilter.js
@@ -9,7 +9,7 @@ hscalc.load();
 //コマンドを読み取り実行結果を得る
 function commandFilter(data) {
   switch (true) {
-    case /> 931/.test(data):
+    case /931/.test(data):
       data = data.replace(/931/g, "");
       data = htmlEscape(data);
       return (data + '<img src="https://pbs.twimg.com/profile_images/510615322307461120/o-vKGUzY_400x400.jpeg" width="100" height="100">');
