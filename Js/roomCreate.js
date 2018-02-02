c2992485fc33536fad3589fb92e6282cb580ccb3
PyMkk <yuyakunnyu@gmail.com>
2018-02-02 18:23:39 +0900

    ルームインフォを送信した

--- Js/roomCreate.js
+++ Js/roomCreate.js
@@ -3,19 +3,27 @@ roomCreateSocket.on('created', (errorMsg) => {
   if (errorMsg == "")
     document.location.href = "/";
   else
-  $("#roomCreateError").prepend("<font color='red' size='3'>"+errorMsg+"</font>");
+    $("#roomCreateError").prepend("<font color='red' size='3'>" + errorMsg + "</font>");
 });
 
 $('#create_room').click(() => {
   let roomName = $("#roomName").val();
   let roomType = $("#roomType").val();
   let description = $("#descText").val();
+  let roomInfo;
   if (roomName == "" || roomType == "")
     return;
   console.log(roomName + ":" + roomType);
+  if (roomType == "discussion_free") {
+    roomInfo = {
+      voteStartTime: 900,
+      voteEndTime: 15
+    };
+  }
   roomCreateSocket.emit('create', JSON.stringify({
     "roomName": roomName,
     "roomType": roomType,
-    "description": description
+    "description": description,
+    "roomInfo": roomInfo
   }));
 });
