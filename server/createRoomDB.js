93824954f5916fbf791e033c5b86cefded76a862
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-02-03 14:42:04 +0900

    creatRoomDB.jsのasync await化

--- server/createRoomDB.js
+++ server/createRoomDB.js
@@ -1,50 +1,33 @@
 const getDbClient = require("./getDbClient.js");
 const debateDB = require("./debateDB.js");
 
-exports.createRoom = (roomName, roomType, description, roomInfo) => {
+exports.createRoom = async (roomName, roomType, description, roomInfo) => {
     const client = getDbClient.get();
     client.connect();
-    return client.query("select count(*) from room where room_name =$1;", [roomName])
-        .then(res => {
-            if (res.rows[0].count != 0)
-                return Promise.reject("すでに部屋が存在します");
-            return Promise.resolve();
-        })
-        .then(() =>
-            client.query(
-                "insert into room(room_name,room_type,description) values($1,$2,$3);",
-                [roomName, roomType, description])
-        )
-        .then(() => client.query("select room_id from room where room_name=$1;", [roomName]))
-        .then(res => {
-            let roomId = res.rows[0].room_id;
-            return createRoomInfo(roomId, roomType, roomInfo);
-        })
-        .then(() => {
-            client.end();
-            return Promise.resolve();
-        })
-        .catch(err => {
-            console.log(err);
-            client.end();
-            return Promise.reject(err);
-        });
+    try {
+        let res = await client.query("select count(*) from room where room_name =$1;", [roomName]);
+        if (res.rows[0].count != 0)
+            throw ("すでに部屋が存在します");
+        await client.query(
+            "insert into room(room_name,room_type,description) values($1,$2,$3);",
+            [roomName, roomType, description]
+        );
+        res = await client.query("select room_id from room where room_name=$1;", [roomName]);
+        let roomId = res.rows[0].room_id;
+        await createRoomInfo(roomId, roomType, roomInfo);
+        client.end();
+    }
+    catch (err) {
+        console.log(err);
+        throw err;
+    }
 }
 
-function createRoomInfo(roomId, roomType, roomInfo) {
+async function createRoomInfo(roomId, roomType, roomInfo) {
     if (roomType == "discussion_free") {
         const client = getDbClient.get();
         client.connect();
-        return debateDB.addDebateInfo(roomId, roomInfo.voteStartTime, roomInfo.voteEndTime)
-            .then((res) => {
-                client.end();
-                return Promise.resolve();
-            })
-            .catch((err) => {
-                client.end();
-                return Promise.reject(err);
-            }
-            );
+        await debateDB.addDebateInfo(roomId, roomInfo.voteStartTime, roomInfo.voteEndTime);
+        client.end();
     }
-    return Promise.resolve();
 }
