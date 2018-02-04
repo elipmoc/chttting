cf0e80663fa56471d54a8ab5c19a5ca8825e89b4
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-02-04 14:29:01 +0900

    movieNameSpaceの名前を修正

--- server/movieSocket.js
+++ server/movieSocket.js
@@ -2,7 +2,7 @@ const escape = require('escape-html');
 
 
 //ディスカッション名前空間にソケットイベントをバインドするクラス
-exports.MovieNameSpace = class {
+exports.movieNameSpace = class {
     constructor(namespace) {
         //ソケットのイベント
         this.connectEvent = (socket) => {
